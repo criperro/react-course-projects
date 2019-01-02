a61591165d2fca206d5b120cd1e00d55bf0ec742
Cristian.Peralta <wombo17@gmail.com>
2019-01-01 19:15:36 -0600

    practice
    practice

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,54 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Does this change!'
+  React.createElement(
+    'h1',
+    null,
+    'Cristian Peralta '
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Cristian Peralta'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 29'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Moravia'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,25 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <h1>Does this change!</h1>;
+var template = (
+    <div>
+      <h1>Cristian Peralta </h1>
+      <p>This is some info</p>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+      <h1>Cristian Peralta</h1>
+      <p>Age: 29</p>
+      <p>Location: Moravia</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
