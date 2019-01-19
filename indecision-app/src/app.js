1b19e26c2b87a7e9dde2e497f4e27921e6c4c21c
criperro <wombo17@gmail.com>
2019-01-18 22:01:57 -0600

    Nested components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render(){
+    return(
+      <div>
+        <Header /> 
+        <Action />   
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render(){
     return (
@@ -24,10 +37,18 @@ class Options extends React.Component {
     return(
       <div>
         <ol>
-          <li>Option 1</li>
-          <li>Option 2</li>
+          Options component here
+          <Option />
         </ol>
       </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render(){
+    return(
+      <div>Option 1</div>
     );
   }
 }
@@ -44,13 +65,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
