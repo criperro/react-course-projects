4edfd603591885c8ab10ea4c0811442911e7ed47
criperro <wombo17@gmail.com>
2019-01-20 19:15:51 -0600

    binding practice

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -39,8 +39,12 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
   handleRemoveAll(){
-    alert('Remove All');
+    console.log(this.props.options);
   }
   render(){
     return(
