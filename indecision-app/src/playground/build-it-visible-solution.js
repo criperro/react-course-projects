a106a9d0b64984c8a31379efd3913a7d5fe00334
Cristian.Peralta <wombo17@gmail.com>
2019-01-26 16:20:09 -0600

    Visibility toggle component practice

--- indecision-app/src/playground/build-it-visible-solution.js
+++ indecision-app/src/playground/build-it-visible-solution.js
@@ -1,26 +1,35 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
 
-const toggleVisibility = () => {
-  visibility = !visibility;
-  render();
-};
+  handleToggleVisibility(){
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
 
-const render = () => {
-  const jsx = (
-    <div>
+  render(){
+    return(
+      <div>
       <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visibility ? 'Hide details' : 'Show details'}
+      <button onClick={this.handleToggleVisibility}>
+        {this.state.visibility ? 'Hide details' : 'Show details'}
       </button>
-      {visibility && (
+      {this.state.visibility && (
         <div>
           <p>Hey. These are some details you can now see!</p>
         </div>
       )}
-    </div>
-  );
+      </div>
+    );
+  }
+}
 
-  ReactDOM.render(jsx, document.getElementById('app'));
-};
-
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
