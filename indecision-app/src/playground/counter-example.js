e37d828b4d29eb6eaabcf8a73ab3a44a7fa8157d
Cristian.Peralta <wombo17@gmail.com>
2019-02-10 19:17:01 -0600

    lifecycle exercise with count

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,9 +5,25 @@ class Counter extends React.Component {
       this.handleMinusOne = this.handleMinusOne.bind(this);
       this.handleReset = this.handleReset.bind(this);
       this.state = {
-        count: props.count
+        count: 0
       };
     }
+
+    componentDidMount(){
+    console.log('didmount');
+      const stringCount = localStorage.getItem('count');
+      const count = parseInt(stringCount,10);
+      if (count && !isNaN(count)) {
+        this.setState(() => ({ count }));
+      }
+    }
+
+    componentDidUpdate(prevProps, prevState){
+      if(prevState.state.count !== this.state.count){
+        localStorage.setItem('count',this.state.count);
+      }
+    }
+
     handleAddOne(){
         this.setState((prevState) => {
           return {
@@ -40,9 +56,5 @@ class Counter extends React.Component {
         );
     }
 }
-
-Counter.defaultProps = {
-    count: 0
-};
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
