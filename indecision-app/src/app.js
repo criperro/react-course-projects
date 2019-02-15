12993457857732f7844d5e6f3d03e346ef54cc6c
Cristian.Peralta <wombo17@gmail.com>
2019-02-15 14:19:26 -0600

    ES6 export exercise

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,9 @@
+import { square, add } from './utils.js';
+import { isAdult, canDrink } from './person.js';
+
 console.log("app.js is running");
+console.log(square(4));
+console.log(add(100, 23));
+
+console.log(isAdult(18));
+console.log(canDrink(16));
---
+++ indecision-app/src/person.js
@@ -0,0 +1,5 @@
+const isAdult = (age) => age >= 18 ? 'Adult!' : 'Not Adult!';
+
+const canDrink = (age) => age >= 18 ? 'Can Drink!' : "Can't Drink!";
+
+export {isAdult,canDrink};
