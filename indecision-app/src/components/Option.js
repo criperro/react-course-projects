751735885ddda96dd03e9f947bb1d24ac236a54f
Cristian.Peralta <wombo17@gmail.com>
2019-02-28 16:03:27 -0600

    Stateless function refactor

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Option = (props) => {
-    return(
-      <p>
-        {props.optionText}
-        <button 
-          onClick={(e) => {
-            props.handleDeleteOption(props.optionText);
-          }}
-        >
-          Remove
-        </button>
-      </p>
-    );
-  };
+const Option = (props) => (
+  <p>
+    {props.optionText}
+    <button 
+      onClick={(e) => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      Remove
+    </button>
+  </p>
+);
 
 export default Option;
