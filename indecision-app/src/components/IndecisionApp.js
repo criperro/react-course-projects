63572250878c4222ecd3c6592693067ab69ed53e
Cristian.Peralta <wombo17@gmail.com>
2019-04-14 18:18:03 -0600

    adding css variables
    css variables

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -71,18 +71,20 @@ export default class IndecisionApp extends React.Component {
       return(
         <div>
           <Header title={title} subtitle={subtitle} /> 
-          <Action 
-            hasOptions={this.state.options.length > 0}
-            handlePick={this.handlePick}
-          ></Action>   
-          <Options 
-            options={this.state.options} 
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption
-            handleAddOption={this.handleAddOption}
-          />
+          <div className="container">
+            <Action 
+              hasOptions={this.state.options.length > 0}
+              handlePick={this.handlePick}
+            ></Action>   
+            <Options 
+              options={this.state.options} 
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
           <OptionModal 
             selectedOption={this.state.selectedOption}
             handleDeleteModal={this.handleDeleteModal}
