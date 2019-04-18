27e7b66f07dbb164e2d2d26d87b6b279b788ba66
Cristian.Peralta <wombo17@gmail.com>
2019-04-18 16:32:44 -0600

    styling modal

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleDeleteModal}
     contentLabel='Selected Option'
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected option:</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleDeleteModal }>Okay</button>
+    <h3 className="modal__title">Selected option:</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleDeleteModal }>Okay</button>
   </Modal>
 );
 
